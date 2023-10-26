d8e5cfcd27960b07079100b89671a0b428df56d3
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2023-10-26 17:46:46 +0530

    Implement Rock, Paper, Scissors Game Logic
    Description:
    
    This commit introduces the fundamental logic for a Rock, Paper, Scissors game. The key aspects of this implementation include:
    
    - Tracking player and computer scores to determine a winner.
    - Generating the computer's choice randomly among rock, paper, and scissors.
    - Enabling button click events for user input.
    - Displaying the updated score on the user interface.
    - Determining and announcing the game winner when a player reaches a score of 5.
    - Implementing a reset mechanism to ensure a fresh game after declaring a winner.

--- app.js
+++ app.js
@@ -1,19 +1,72 @@
-// Get input from the user
-const userChoice = prompt("Choose: rock, paper or scissors");
+let playerScore = 0;
+let computerScore = 0;
+const resultsElement = document.getElementById( "results");
+
 // Generate computer's choice
-const strings = ["rock", "paper", "scissors"];
-const randomIndex = Math.floor(Math.random() * strings.length);
-const randomString = strings[randomIndex];
-// Compare the user and computer's choice
-if (userChoice === randomString) {
-    console.log("The game is draw");
-} else if (userChoice === "paper" && randomString === "rock") {
-    console.log("You won!");
-} else if (userChoice === "rock" && randomString === "scissors") {
-    console.log("You won");
-} else if (userChoice === "scissors" && randomString === "paper") {
-    console.log("You won!");
-} else {
-    console.log("Computer won");
+const computerSelection = getComputerChoice();
+function getComputerChoice() {
+    const strings = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * strings.length);
+    const randomString = strings[randomIndex];
+    return randomString;
 }
-// Display the choices
+
+// UI Logic
+function handleButtonClick(event) {
+    const playerSelection = event.target.id;
+    playRound(playerSelection);
+    const result = playRound(playerSelection, computerSelection);
+    
+    if (result === "You won") {
+        playerScore++;
+    }
+    else if (result === "Computer won") {
+        computerScore++;
+    }
+
+    updateScore();
+    checkForWinner();
+
+}
+
+function updateScore() {
+    const scoreElement = document.getElementById("score");
+    scoreElement.textContent = `Player: ${playerScore} - Computer ${computerScore}`;
+}
+
+function checkForWinner() {
+    if (playerScore === 5) {
+        resultsElement.textContent = "Player wins the game!";
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore === 5) {
+        resultsElement.textContent = "Computer wins the game!";
+        playerScore = 0;
+        computerScore = 0;
+    }
+}
+
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+
+rockButton.addEventListener("click", handleButtonClick);
+paperButton.addEventListener("click", handleButtonClick);
+scissorsButton.addEventListener("click", handleButtonClick);
+
+// Compare the user and computer's choice
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "The game is draw";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You won!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You won";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You won!";
+    } else {
+        return "Computer won";
+    }
+}
+
